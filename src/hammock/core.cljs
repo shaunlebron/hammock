5e3eec81c3979c982b202fad3da0385f260b4137
Shaun Williams <shaunewilliams@gmail.com>
2014-12-17 19:37:39 -0600

    add IHammock, Hammock, create, and implement lookup

--- src/hammock/core.cljs
+++ src/hammock/core.cljs
@@ -1,2 +1,32 @@
 (ns hammock.core)
 
+(defprotocol IHammock
+  (-src [h])
+  (-dst [h])
+  (-anchors [h])
+  (-set! [h dst-key src-key]
+         [h dst-key src-key d-fn])
+  (-nest! [h dst-key src-key]
+          [h dst-key src-key h-fn])
+  (-map! [h dst-key src-key]
+         [h dst-key src-key h-fn])
+  (-man! [h dst-ks value]
+         [h dst-ks value src-keys]))
+
+(deftype Hammock [src dst anchors]
+  ILookup
+  (-lookup [this k]
+    (-lookup this k nil))
+  (-lookup [this k not-found]
+    (-lookup src k not-found)))
+
+(defn create
+  ([src] (create src (atom {}) (atom {})))
+  ([src dst] (create src dst (atom {})))
+  ([src dst anchors]
+   (Hammock. src dst anchors)))
+
+;; (set! h :dest-key :src-key fn?)
+;; (nest! h :dest-key :src-key h-fn)
+;; (map! h :dest-key :src-key h-fn)
+;; (man! h :dest-key fn :src-keys)
--- test/hammock/tests.cljs
+++ test/hammock/tests.cljs
@@ -1,11 +1,14 @@
 (ns hammock.tests
   (:require
+    [hammock.core :as hm]
     [cljs.test :refer-macros [deftest is testing run-tests]]))
 
 (deftest test-basic
-  (testing "Testing basic arithmetic"
-    (is (= 2 (+ 1 1)))
-    (is (= 6 (* 3 2)))))
+  (testing "Testing lookup on destination anchor."
+    (let [src {:foo "bar" :bar ["hi" "there"]}
+          h (hm/create src)]
+      (is (= "bar" (:foo h)))
+      (is (= "there" (get-in h [:bar 1]))))))
 
 (enable-console-print!)
 
